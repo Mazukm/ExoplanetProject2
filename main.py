1955a82eca2ac1c63cfabbdbfc489ff450d83795
Mazukm <105970409+Mazukm@users.noreply.github.com>
2022-06-07 00:32:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-import numpy as np
+"""
+main data file, that runs all
+of the code needed
+"""
+
 import pickle
 import data_import as di
 import data_graph as dg
@@ -19,10 +22,12 @@ def main():
     dg.DataGraph.plot_counts_spectrum(di.DataImport.avrg_num_spectrum(df))
     dg.DataGraph.plot_counts_metalicity(di.DataImport.avrg_num_metalicity(df))
     labels, features = ml.MachineLearning.prep_data(df)
-    features_train, features_test, labels_train, labels_test = ml.MachineLearning.seperate_datasets(labels, features)
+    features_train, features_tst, labels_train, labels_tst = (
+        ml.MachineLearning.seperate_datasets(labels, features))
     model = ml.MachineLearning.train_model(labels_train, features_train)
     pickle.dump(model, open("model.p", "wb"))
-    ml.MachineLearning.test_model(labels_test, features_test, model)
+    ml.MachineLearning.test_model(labels_tst, features_tst, model)
+
 
 if __name__ == "__main__":
     main()
