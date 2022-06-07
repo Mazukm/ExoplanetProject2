1907f33fc4c8a3ff41a5b0dd3f8d78e198e39d17
Mazukm <105970409+Mazukm@users.noreply.github.com>
2022-06-07 00:36:42 -0700

    Update data_graph.py

--- data_graph.py
+++ data_graph.py
@@ -1,15 +1,22 @@
-import pandas as pd
-import numpy as np
+"""
+CSE 163
+
+using the gicen data frames
+creats a seriese of graphs
+"""
+
 import seaborn as sbn
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
 
 class DataGraph():
+
     def plot_counts_metalicity(df):
         """
         makes a boxplot of the metal ratios
         to the amount of planets
         """
-        sbn.catplot(x="st_metratio", kind='bar', y="sy_pnum", data=df) 
+        sbn.catplot(x="st_metratio", kind='bar', y="sy_pnum", data=df)
         plt.xlabel("Metalicity ratio of the star")
         plt.ylabel("number of planets")
         plt.title("Metalicity of the star to the number of planets")
@@ -22,7 +29,7 @@ class DataGraph():
         makes a boxplot of the spectrum
         to the avrage amount of planets
         """
-        sbn.catplot(x="st_spectype", kind='bar', y="sy_pnum", data=df) 
+        sbn.catplot(x="st_spectype", kind='bar', y="sy_pnum", data=df)
         plt.xlabel("Spectrum of the star")
         plt.ylabel("number of planets")
         plt.title("Spectrum of the star to the number of planets")
@@ -35,13 +42,13 @@ class DataGraph():
         makes a scatterplot of number of planets
         to the log of the tempurature of the star
         """
-        sbn.regplot(x="st_logg", y="sy_pnum", data=df) 
+        sbn.regplot(x="st_logg", y="sy_pnum", data=df)
         plt.xlabel("log mass of the star (Kg)")
         plt.ylabel("number of planets")
         plt.title("log mass of the star to the number of planets")
         plt.savefig("./graphdata/mass_planet_count.png")
         plt.clf()
-    
+
     def plot_counts_temp(df):
         """
         makes a scatter plot with the tempurature
@@ -54,7 +61,6 @@ class DataGraph():
         plt.savefig("./graphdata/amount_to_tempurature.png")
         plt.clf()
 
-
     def plot_counts_distance(df):
         """
         makes a scatter plot with the tempurature
@@ -63,10 +69,10 @@ class DataGraph():
         sbn.regplot(x="sy_dist", y="sy_pnum", data=df)
         plt.xlabel("Distance to the planet (pc)")
         plt.ylabel("number of planets")
-        plt.title("Number of the planets to the distance from the solar system")
+        plt.title("Number of the planets to distance from the solar system")
         plt.savefig("./graphdata/amount_to_distance.png")
         plt.clf()
-    
+
     def plot_counts_rad(df):
         """
         makes a scatterplot of radius to planet number
