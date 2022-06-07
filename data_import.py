685f3b8b60f10e768f6a97085f45d7eb90d6147d
Mazukm <105970409+Mazukm@users.noreply.github.com>
2022-06-06 20:25:15 -0700

    Add files via upload

--- data_import.py
+++ data_import.py
@@ -1,38 +1,42 @@
 import pandas as pd
 import numpy as np
+import os
 
 class DataImport():
 
     def filter_columns(df):
+        """
+        filters the columns down and returns on the 
+        """
         df = df.loc[:,["hostname","st_teff","st_rad","sy_snum","st_metratio"
-                        ,"st_spectype","st_logg","sy_pnum"]]
+                        ,"st_spectype","st_logg","sy_pnum","sy_dist"]]
         df = df[df["sy_snum"] == 1]
-        return df.dropna()
+        df = df.dropna()
+        return df[(df.T != 0).any()].drop_duplicates()
 
-    def dedupe(df):
-        return df.drop_duplicates()
-    
+
     def avrg_num_metalicity(df):
-        df = df.groupby("st_metratio", as_index=False)["sy_pnum"].mean()
-        print(df)
-        return df
-    
-    def avrg_num_temp(df):
-        return df.groupby("st_teff", as_index=False)["sy_pnum"].mean()
+        """
+        calculates the avrage number of planets according to each metalicity column
+        """
+        return df[df["st_metratio"] != "[Fe/H["].groupby("st_metratio", as_index=False)["sy_pnum"].mean()
+
 
     def avrg_num_spectrum(df):
+        """
+        creates a dataset of the mean of planets per spectrum type
+        """
         return df.groupby("st_specty", as_index=False)["sy_pnum"].mean()
 
-    def avrg_num_mass(df):
-        return df.groupby("st_metratio", as_index=False)["sy_pnum"].mean()
 
     def read_in_data():
         """
         Imports and does high level filtering of the dataset
         """
-        data_frame1 = pd.read_csv("PS_2022.05.23_16.34.26.csv", skiprows=99)
-        data_frame2 = pd.read_csv("PS_2022.05.23_16.36.33.csv", skiprows=96)
-        full_data_frame = data_frame1.append(data_frame2, ignore_index=True)
+        current_dir = os.getcwd()
+        data_frame1 = pd.read_csv(current_dir + "/" + "PS_2022.05.23_16.34.26.csv", skiprows=99)
+        data_frame2 = pd.read_csv(current_dir + "/" + "PS_2022.05.23_16.36.33.csv", skiprows=96)
+        full_data_frame = pd.concat([data_frame1, data_frame2])
         full_data_frame = full_data_frame[(full_data_frame["pl_controv_flag"] == 0 )
                                           | (full_data_frame["pl_controv_flag"] == "0")]
         return full_data_frame
